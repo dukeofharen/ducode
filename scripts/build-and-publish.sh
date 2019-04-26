059ffd8e8196fb08de43fef2dec11f63fa405a50
Duco <duco@winterwerp.it>
2019-04-26 22:48:32 +0200

    Added vagrant file and updated build-and-publish.sh script

--- scripts/build-and-publish.sh
+++ scripts/build-and-publish.sh
@@ -2,6 +2,27 @@
 set -e
 set -u
 
+HASH_PATH=/tmp/ducode_hash
+
+# Do a git pull and check if a new commit is available
+git pull
+git submodule update --remote
+
+OLD_HASH=""
+CURRENT_HASH=$(git rev-parse --short HEAD)
+
+if [ -f "$HASH_PATH" ]; then
+    OLD_HASH=$(cat $HASH_PATH)
+fi
+
+if [ "$OLD_HASH" != "$CURRENT_HASH" ]; then
+    echo "Old hash is '$OLD_HASH' and current hash is '$CURRENT_HASH'. Rebuilding site."
+    echo $CURRENT_HASH > $HASH_PATH
+else
+    echo "Old hash '$OLD_HASH' and current hash '$CURRENT_HASH' are the same. Not rebuilding site."
+    exit 0
+fi
+
 export HUGO_ENV=production
 cd ..
 hugo
---
+++ scripts/vagrant/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+
+# Install dependencies
+sudo apt-get install -y snapd nginx
+
+# Install Hugo
+sudo snap install
